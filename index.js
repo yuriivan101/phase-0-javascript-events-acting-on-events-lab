1cb70f01ee1ab38961513111eb06e0390cdb68de
Yuriy <ivanenko.yuri@gmail.com>
2024-02-24 20:28:04 -0500

    Moved right and left

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px","");
+    console.log(`${rightNumbers}`)
+    const right = parseInt(rightNumbers,  10);
+    if(right < 360){
+        dodger.style.left = `${right + 5}px`
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        console.log('ArrowRight')
+        moveDodgerRight();
+    }
+  });
